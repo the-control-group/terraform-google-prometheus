0e30c2b8d8c67516527bf569e17ed8e46f1a32bc
Mike Keller <mike.keller@thecontrolgroup.com>
2018-05-30 15:25:24 -0700

    Formatting.

--- main.tf
+++ main.tf
@@ -1,51 +1,55 @@
 resource "google_compute_disk" "prometheus-disk" {
-    name = "${var.deployment_name}-disk"
-    type = "pd-standard"
-    zone = "${var.gcp_zone}"
-    size = "20"
-    image = "${var.disk_image}"
+  name  = "${var.deployment_name}-disk"
+  type  = "pd-standard"
+  zone  = "${var.gcp_zone}"
+  size  = "20"
+  image = "${var.disk_image}"
 }
 
 resource "google_compute_instance" "prometheus-server" {
-    name = "${var.deployment_name}-server"
-    machine_type = "${var.machine_type}"
-    zone = "${var.gcp_zone}"
+  name         = "${var.deployment_name}-server"
+  machine_type = "${var.machine_type}"
+  zone         = "${var.gcp_zone}"
 
-    tags = "${var.server_tags}"
+  tags = "${var.server_tags}"
 
-    boot_disk {
-        source = "${var.deployment_name}-disk"
-        auto_delete = false
-    }
+  boot_disk {
+    source      = "${var.deployment_name}-disk"
+    auto_delete = false
+  }
 
-    network_interface {
-        subnetwork = "${var.gcp_subnetwork}"
-    }
+  network_interface {
+    subnetwork = "${var.gcp_subnetwork}"
+  }
 
-    service_account {
-            scopes = "${var.server_scopes}"
-    }
+  service_account {
+    scopes = "${var.server_scopes}"
+  }
 }
 
 resource "google_compute_firewall" "prometheus-server-fw-9090" {
-    name = "${var.deployment_name}-server-9090"
-    network = "${var.gcp_network}"
-    allow {
-        protocol = "tcp"
-        ports = ["9090"]
-    }
-    source_ranges = "${var.server_source_ranges}"
-    source_tags = "${var.server_source_tags}"
+  name    = "${var.deployment_name}-server-9090"
+  network = "${var.gcp_network}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9090"]
+  }
+
+  source_ranges = "${var.server_source_ranges}"
+  source_tags   = "${var.server_source_tags}"
 }
 
 resource "google_compute_firewall" "prometheu-node-exporter-fw-9100" {
-    name = "${var.deployment_name}-node-exporter-9100"
-    project = "${var.gcp_project}"
-    network = "${var.gcp_network}"
-    allow {
-        protocol = "tcp"
-        ports = ["9100"]
-    }
-    source_ranges = "${var.node_exporter_ranges}"
-    source_tags = "${var.node_exporter_tags}"
+  name    = "${var.deployment_name}-node-exporter-9100"
+  project = "${var.gcp_project}"
+  network = "${var.gcp_network}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9100"]
+  }
+
+  source_ranges = "${var.node_exporter_ranges}"
+  source_tags   = "${var.node_exporter_tags}"
 }
--- variables.tf
+++ variables.tf
@@ -1,74 +1,74 @@
 /// Required
 variable "gcp_project" {
-    description = "The project that prometheus will be deployed to."
-    type = "string"
+  description = "The project that prometheus will be deployed to."
+  type        = "string"
 }
 
 variable "gcp_zone" {
-    description = "The host zone where prometheus will be deployed to."
-    type = "string"
+  description = "The host zone where prometheus will be deployed to."
+  type        = "string"
 }
 
 variable "gcp_network" {
-    description = "Network that the firewall rules will be associated with."
-    type = "string"
+  description = "Network that the firewall rules will be associated with."
+  type        = "string"
 }
 
 variable "gcp_subnetwork" {
-    description = "Subnetwork to attach the instance to."
-    type = "string"
+  description = "Subnetwork to attach the instance to."
+  type        = "string"
 }
 
 variable "disk_image" {
-    description = "Disk image to use when deploying prometheus. This module assumes that you have already deployed an image that has prometheus installed and configured."
-    type = "string"
+  description = "Disk image to use when deploying prometheus. This module assumes that you have already deployed an image that has prometheus installed and configured."
+  type        = "string"
 }
 
 /// Optional
 variable "deployment_name" {
-    description = "Name to identify the prometheus deployement."
-    type = "string"
-    default = "prometheus"
+  description = "Name to identify the prometheus deployement."
+  type        = "string"
+  default     = "prometheus"
 }
 
 variable "machine_type" {
-    description = "Instance size to deploy prometheus on to."
-    type = "string"
-    default = "g1-small"
+  description = "Instance size to deploy prometheus on to."
+  type        = "string"
+  default     = "g1-small"
 }
 
 variable "server_tags" {
-    description = "Network tags to identify the server."
-    type = "list"
-    default = ["prometheus", "prometheus-server", "consul-client"]
+  description = "Network tags to identify the server."
+  type        = "list"
+  default     = ["prometheus", "prometheus-server", "consul-client"]
 }
 
 variable "server_scopes" {
-    description = "The instance's permissions to different GCP services."
-    type = "list"
-    default = ["compute-ro", "logging-write", "storage-rw", "monitoring"]
+  description = "The instance's permissions to different GCP services."
+  type        = "list"
+  default     = ["compute-ro", "logging-write", "storage-rw", "monitoring"]
 }
 
 variable "server_source_ranges" {
-    description = "The source ranges allowed access to communicate with the prometheus server."
-    type = "list"
-    default = []
+  description = "The source ranges allowed access to communicate with the prometheus server."
+  type        = "list"
+  default     = []
 }
 
 variable "server_source_tags" {
-    description = "The source ranges allowed access to communicate with the prometheus server."
-    type = "list"
-    default = []
+  description = "The source ranges allowed access to communicate with the prometheus server."
+  type        = "list"
+  default     = []
 }
 
 variable "node_exporter_ranges" {
-    description = "The source ranges allowed to access to communicate with the node exporter."
-    type = "list"
-    default = []
+  description = "The source ranges allowed to access to communicate with the node exporter."
+  type        = "list"
+  default     = []
 }
 
 variable "node_exporter_tags" {
-    description = "The source tags allowed to access to communicate with the node exporter."
-    type = "list"
-    default = []
+  description = "The source tags allowed to access to communicate with the node exporter."
+  type        = "list"
+  default     = []
 }
